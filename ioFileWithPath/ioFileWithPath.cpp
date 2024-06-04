0f392e670c4a7739437a57c68799606e91e34e4f
nachool <nachma.olivia1394@gmail.com>
2024-06-04 17:48:47 +0700

    jika ada file maka

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -27,3 +27,28 @@ int main()
         outfile << baris << endl;
     }
 
+    // selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    // membuka file dalam mode membaca
+    ifstream infile;
+    // menunjuk ke sebuah file
+    infile.open(NamaFile + ".txt", ios::out);
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    // jika file ada maka
+    if (infile.is_open())
+    {
+        // melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            // dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        // tutup file tersebut setelah selesai
+        infile.close();
+    }
+    // jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
+}

17ab1a15baec9d61c1f9a1e4b7e5b341bd7b6519
nachool <nachma.olivia1394@gmail.com>
2024-06-04 17:46:25 +0700

    menulis file

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -7,3 +7,10 @@ int main()
 {
     string baris;
     string NamaFile;
+
+    // membuka file dalam mode menulis.
+    ofstream outfile;
+    // menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis File, \'q\' untuk keluar" << endl;
